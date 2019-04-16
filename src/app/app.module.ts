9dfa292b4be3ecd14957e0eb99e83b27eceecca1
MUSIALIK Przemyslaw <przemyslaw.musialik@soprasteria.com>
2019-04-16 11:24:42 +0200

    Add post-create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
